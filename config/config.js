3930c4218775793f6b9606057479bcc649e61a73
Jvilchezru <vilchez.10.2001@gmail.com>
2023-07-05 06:11:38 -0500

    new bd 2

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const mysql = require('promise-mysql');
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: process.env.FB_USERNAME,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DBNAME
 });
